a4df8648923f4203120e0de5ac7956ff82c83a86
Masoom Kumar <masoom.kr04@gmail.com>
2021-09-23 17:09:55 +0530

    sidebar component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,79 @@
 import React from "react";
 import styled from "styled-components";
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import ChatIcon from "@material-ui/Icons/chat";
+import SearchIcon from "@material-ui/Icons/search";
+import { MoreVert } from "@material-ui/icons";
 
 export const Sidebar = () => {
   return (
     <Container>
-      <h2>Whatsapp</h2>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+
+      <SidebarButton>Start a new chat</SidebarButton>
     </Container>
   );
 };
 
 const Container = styled.div``;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+  margin-left: 5px;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from "next/head";
 import { Sidebar } from "../components/Sidebar";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Whatsapp-clone</title>
         <link rel="icon" href="/favicon.ico" />
