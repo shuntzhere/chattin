2ed2f0c4d2f0efd68917792a2991113f826563b3
Masoom Kumar <masoom.kr04@gmail.com>
2021-09-23 05:41:42 +0530

    Initial commit

---
+++ components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Sidebar = () => {
+  return (
+    <Container>
+      <h2>Whatsapp</h2>
+    </Container>
+  );
+};
+
+const Container = styled.div``;
