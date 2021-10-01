c2bcb5e49085417c6a906268e89a235a6b675309
Masoom Kumar <masoom.kr04@gmail.com>
2021-10-01 05:41:31 +0530

    id component

---
+++ components/ChatScreen.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ChatScreen = () => {
+  return (
+    <Container>
+      <h1>chat</h1>
+    </Container>
+  );
+};
+
+const Container = styled.div``;
